fdf04fb5576a6d386cc8c84528748468010c8f70
Vinicius Augusto <vinicius_augusto_@msn.com>
2023-01-07 13:17:03 -0300

    Introduction to reactive programming

---
+++ src/main/java/com/vinicius/reactiveexamples/PersonRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.vinicius.reactiveexamples;
+
+import com.vinicius.reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class PersonRepositoryImpl implements PersonRepository {
+
+    Person michael = new Person(1, "Michael", "Weston");
+    Person fiona = new Person(2, "Fiona", "Gleanne");
+    Person sam = new Person(3, "Sam", "Axe");
+    Person jesse = new Person(4, "Jesse", "Porter");
+
+    @Override
+    public Mono<Person> getById(final Integer id) {
+        return findAll().filter(person -> person.getId().equals(id)).next();
+    }
+
+    @Override
+    public Flux<Person> findAll() {
+        return Flux.just(michael, fiona, sam, jesse);
+    }
+}
